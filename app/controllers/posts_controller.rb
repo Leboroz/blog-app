bdf9447ca48f17b0b880b772ed0ecbfde9b595ec
Leonardo <albornozleo95@gmail.com>
2022-05-05 13:57:16 -0400

    Create post new and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    post_values = params.require(:post).permit(:title, :text)
+    @new_post = Post.new(author: user, title: post_values[:title], text: post_values[:text])
+    respond_to do |format|
+      format.html do
+        if @new_post.save
+          flash[:success] = 'post saved successfully'
+          redirect_to user_posts_path(user.id)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new
+        end
+      end
+    end
+  end
+
   def index
     @user = User.find(params[:user_id])
     @button_text = 'Pagination'
