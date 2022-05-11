590ff0ab8b369b6eb9bdb2dda880853daf612a2e
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-11 13:00:15 -0400

    Update project views

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   post 'users/:user_id/posts/:post_id', to: 'comments#create', as: 'new_comment' # comments create
   post 'users/:user_id/posts/:post_id/like', to: 'likes#create', as: 'new_like' # likes create
   delete 'users/:user_id/posts/:post_id', to: 'posts#delete', as: 'delete_post'
-	delete 'delete/users/:user_id/posts/:id/comment/:comment_id', to: 'comments#delete', as: 'delete_comment'         
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new]
+    resources :posts, only: [:index, :show, :new] do
+			resources :comments, only: [:destroy]
+		end
   end
 end
