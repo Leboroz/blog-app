690def6831e371dd79955dd823091ed8dd587ae8
Leonardo <albornozleo95@gmail.com>
2022-05-11 19:52:58 -0400

    Add test for user#show

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -36,9 +36,48 @@ RSpec.describe "Users", type: :system do
 				
 				expect(current_path).to eq("/users/#{@user.id}")
 			end
-
 		end
 
-		
+    describe '#show' do
+      before :each do
+        visit user_path(@user.id)
+      end
+
+      it 'should display user profile picture' do
+        expect(page).to have_xpath(".//img[@src=\"#{@user.photo}\"]")
+      end
+
+      it 'should display user\'s post_counter' do
+				expect(page).to have_content("Number of posts: #{@user.post_counter}")
+      end
+
+      it 'should display user\'s bio' do
+				expect(page).to have_content('Bio')
+      end
+
+      it 'should display user\'s most recent posts' do
+        @user.most_recent_posts.each { |post| expect(page).to have_content(post.title) }
+      end
+
+      it 'should display a button that leads to view all posts' do
+				expect(page).to have_content('See all posts')
+      end
+
+      context 'When I click on' do
+        it 'user\'s post should redirected to show page' do
+          post = @user.posts.last
+
+          click_link(post.title)
+
+          expect(current_path).to eql "/users/1/posts/#{post.id}"
+        end  
+
+        it 'See all posts should redirected to show page' do
+          click_link('See all posts')
+
+          expect(current_path).to eql "/users/#{@user.id}/posts"
+        end  
+      end
+    end
 	end
 end
